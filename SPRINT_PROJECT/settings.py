52648876861710e57bf8c7cb35bc74192f09a037
Jafaru Idris Tesla <57459852+jafarjtown@users.noreply.github.com>
2022-09-16 18:11:47 +0100

    Update settings.py

--- SPRINT_PROJECT/settings.py
+++ SPRINT_PROJECT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(8*upz$@d^=%d@vk^bkt$bf!6*rsi6avsa60k5p#pn+!^zm4$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sprintproject-production.up.railway.app', '*']
 
 
 # Application definition
